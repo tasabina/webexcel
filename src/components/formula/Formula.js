01943816e02c8971d8eca108780ad72c28c0d06f
tasabina <tasabina@yandex.ru>
2020-06-19 22:21:45 +1200

    Finish local storage functionality

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -8,6 +8,7 @@ export class Formula extends ExcelComponent {
     super($root, {
       name: 'Formula',
       listeners: ['input', 'keydown'],
+      subscribe: ['currentText', 'colState'],
       ...options,
     })
   }
@@ -15,11 +16,9 @@ export class Formula extends ExcelComponent {
   init() {
     super.init();
     this.$formula = this.$root.find('#formula');
+
     this.$on('table:select', ($cell) => {
-      this.$formula.text($cell.text());
-    });
-    this.$on('table:input', ($cell) => {
-      this.$formula.text($cell.text());
+      this.$formula.text($cell.data.value);
     });
   }
 
@@ -27,6 +26,10 @@ export class Formula extends ExcelComponent {
     return `<div class="info">fx</div>
     <div id="formula" class="input" contenteditable="true" spellcheck="false">
     </div>`;
+  }
+
+  storeChanged({currentText}) {
+    this.$formula.text(currentText);
   }
 
   onInput(event) {
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,6 +4,8 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.store = options.store;
+    this.subscribe = options.subscribe || [];
     this.emitter = options.emitter;
     this.unsubscribers = [];
 
@@ -26,6 +28,16 @@ export class ExcelComponent extends DomListener {
   $on(event, fn) {
     const unsub = this.emitter.subscribe(event, fn);
     this.unsubscribers.push(unsub);
+  }
+
+  $dispatch(action) {
+    this.store.dispatch(action);
+  }
+
+  storeChanged() {}
+
+  isWatching(key) {
+    return this.subscribe.includes(key);
   }
 
   init() {
